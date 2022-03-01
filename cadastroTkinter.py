c83f2ade62f7ba150bc16d17388768272eaefdaa
ssouz <81429731+ssouz@users.noreply.github.com>
2022-03-01 11:55:52 -0300

    Update cadastroTkinter.py

--- cadastroTkinter.py
+++ cadastroTkinter.py
@@ -4,11 +4,29 @@ import tkinter as tk
 from tkinter import ttk
 import datetime as dt
 
+
 list_types = ["galao", "caixa", "saco", "unidade"]
+list_codes = []
+
+# https://youtu.be/TXlkiMIBlTM?t=2367
+# criacao da funçao
+
+
+def insert_code():
+    description = entry_description.get()
+    type = ComboBox_select_type.get()
+    amount = entry_amount.get()
+    create_date = dt.datetime.now()
+    create_date = create_date.strftime("%d/%m/%Y %H:%M")
+    code = len(list_codes)+1
+    code_str = "COD-{}".format(code)
+    list_codes.append((code_str, description, type, amount, create_date))
+
 
 window = tk.Tk()
 
 window.title('ferramenta de cadastro de materiais')
+
 
 label_description = tk.Label(text="Descriçao do material")
 label_description.grid(row=1, column=0, padx=10, pady=10,
@@ -30,13 +48,15 @@ label_amount = tk.Label(text="Quantidade na unidade de material")
 label_amount.grid(row=4, column=0, padx=10, pady=10,
                   sticky='nswe', columnspan=2)
 
-label_amount = tk.Entry()
-label_amount.grid(row=4, column=2, padx=10, pady=10,
+entry_amount = tk.Entry()
+entry_amount.grid(row=4, column=2, padx=10, pady=10,
                   sticky='nswe', columnspan=2)
 
-button_create_code = tk.Button(text="Criar codigo")
+button_create_code = tk.Button(text="Criar codigo", command=insert_code)
 button_create_code.grid(row=5, column=0, padx=10,
                         pady=10, sticky='nswe', columnspan=4)
 
 
 window.mainloop()
+
+print(list_codes)
